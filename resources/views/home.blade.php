e9462904f7f0baaa4384bd8ca5b67bee3f7d0a66
iamfre <zycs@yandex.ru>
2023-03-12 01:32:07 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Главная страница')
+@include('partials.header')
+@section('content')
+
+    <h1>Hello</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title', config('app.name'))</title>
+
+    <link href="/css/app.css" rel="stylesheet">
+    @stack('styles')
+</head>
+<body>
+    <header>
+        @yield('header')
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        @yield('footer')
+    </footer>
+    <script src="/js/app.js"></script>
+    @stack('scripts')
+</body>
+</html>
