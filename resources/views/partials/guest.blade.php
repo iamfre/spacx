e5edeb486a07c72c1230195be5906e3336e218d3
iamfre <zycs@yandex.ru>
2023-03-15 15:17:20 +0300

    refactor header

---
+++ resources/views/partials/guest.blade.php
@@ -0,0 +1,17 @@
+<a class="text-gray-500 hover:text-orange" href="{{ route('registration') }}">
+    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4"
+         fill="currentColor" viewBox="0 0 26 26" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+    </svg>
+    Регистрация
+</a>
+<a class="text-gray-500 hover:text-orange" href="{{ route('login') }}">
+    <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4"
+         viewBox="0 0 22 22" fill="currentColor">
+        <path fill-rule="evenodd"
+              d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+              clip-rule="evenodd"/>
+    </svg>
+    Войти
+</a>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,69 +3,18 @@
         <div class="container mx-auto block overflow-hidden px-4 sm:px-6 sm:flex sm:justify-between sm:items-center py-1 space-y-4 sm:space-y-0">
             <div class="flex justify-center">
                 <a href="{{ route('home') }}" class="inline-block sm:inline hover:opacity-75">
-                    <img src="{{ asset("storage/logo/logo-6.png") }}" width="100" height=auto alt="SPACX">
+                    <img src="{{ asset("storage/logo/logo.png") }}" width="100" height=auto alt="SPACX">
                 </a>
             </div>
-            <div>
+            <div class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
                 @auth()
-                    <nav class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
-                        <a class="text-black hover:text-orange" href="#">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                            </svg>
-                            <span class="inline-block font-bold">{{ auth()->user()->name }}</span>
-                        </a>
-                        <a class="text-gray-500 hover:text-orange" href="{{ route('logout') }}">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-                            </svg>
-                            Выйти
-                        </a>
-                    </nav>
+                    @include('partials.auth')
                 @endauth
-
                 @guest()
-                    <ul class="flex justify-center sm:justify-end items-center space-x-8 text-sm">
-                        <li>
-                            <a class="text-gray-500 hover:text-orange" href="{{ route('registration') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4"
-                                     fill="currentColor" viewBox="0 0 26 26" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-                                </svg>
-                                Регистрация
-                            </a>
-                        </li>
-                        <li>
-                            <a class="text-gray-500 hover:text-orange" href="{{ route('login') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="inline-block text-orange h-4 w-4"
-                                     viewBox="0 0 22 22" fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                          d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                                Войти
-                            </a>
-                        </li>
-                    </ul>
+                    @include('partials.guest')
                 @endguest
             </div>
         </div>
     </div>
-    <div class="border-b">
-        <div class="container mx-auto overflow-hidden px-4 sm:px-6">
-            <section class="bg-white py-4">
-                <ul class="list-inside bullet-list-item flex flex-wrap justify-between -mx-5 -my-2">
-                    <li class="px-5 py-2"><a class="{{ Request::is('/') ? 'text-orange cursor-default' : 'text-gray-600 hover:text-orange' }}" href="{{ route('home') }}">Главная</a></li>
-                    <li class="px-5 py-2"><a class="{{ Request::is('article') ? 'text-orange cursor-default' : 'text-gray-600 hover:text-orange' }}" href="#">Статьи</a></li>
-                    <li class="px-5 py-2"><a class="{{ Request::is('catalog') ? 'text-orange cursor-default' : 'text-gray-600 hover:text-orange' }}" href="{{ route('catalog.index') }}">Каталог</a></li>
-                    <li class="px-5 py-2"><a class="{{ Request::is('contacts') ? 'text-orange cursor-default' : 'text-gray-600 hover:text-orange' }}" href="{{ route('contacts') }}">Контакты</a></li>
-                </ul>
-            </section>
-        </div>
-    </div>
+    @include('partials.nav')
 </header>
