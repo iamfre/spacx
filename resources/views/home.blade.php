6f7dd04751f269a281f7ae262424b6929dfaeeee
iamfre <zycs@yandex.ru>
2023-03-15 15:33:51 +0300

    small fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
     <main class="flex-1 container mx-auto bg-white overflow-hidden px-4 sm:px-6">
-        <div class="py-4 pb-8">
+        <div class="py-4 pb-8 pt-6">
             <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6">
                 @each('partials.ships.item', $ships, 'ship')
             </div>
