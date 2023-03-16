b6ed6d142faa60ec1a033c822bc55a5b224ba699
iamfre <zycs@yandex.ru>
2023-03-16 21:13:55 +0300

    fix route middleware

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/registration', [AuthController::class, 'showRegisterForm'])->name('registration');
+    Route::post('/registration', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/contacts', [IndexController::class, 'contacts'])->name('contacts');
-
-Route::get('/registration', [AuthController::class, 'showRegisterForm'])->name('registration');
-Route::post('/registration', [AuthController::class, 'register']);
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile');
-
 Route::get('/catalog', [CatalogController::class, 'index'])->name('catalog.index');
 Route::get('/catalog{id}', [CatalogController::class, 'show'])->name('catalog.show');
-
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
