e1e8d1b7f667d61788bec73ce826b0ffbc8db91a
iamfre <zycs@yandex.ru>
2023-03-21 12:49:16 +0300

    fix: item link

--- resources/views/partials/ships/item.blade.php
+++ resources/views/partials/ships/item.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white w-full border border-gray-100 rounded overflow-hidden shadow-lg hover:shadow-2xl">
     <a class="block w-full" href="{{ route('catalog.show', $ship->id) }}"><img class="w-full h-full hover:opacity-90 object-cover" src="{{ asset("storage/catalog/$ship->image") }}" alt="{{ $ship->name }}"></a>
     <div class="px-6 py-4">
-        <div class="text-black font-bold text-xl mb-2"><a class="hover:text-orange" href="#">{{ $ship->name }}</a></div>
+        <div class="text-black font-bold text-xl mb-2"><a class="hover:text-orange" href="{{ route('catalog.show', $ship->id) }}">{{ $ship->name }}</a></div>
         <p class="text-grey-darker text-base">
             <span class="inline-block">{{ $ship->price }} $</span>
             @if($ship->old_price)
